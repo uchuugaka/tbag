a120551edff6a8df43736cfe138a0545528fe251
huangtao <huangtao@klicen.com>
2017-09-26 10:48:29 +0800

    异步http请求增加请求超时时间

--- tbag/utils/http_client.py
+++ tbag/utils/http_client.py
@@ -7,6 +7,7 @@ Author: huangtao
 Date:   2017/07/31
 UPDATE:
     2017/09/19  增加对POST请求body的编码格式区分
+    2017/09/26  增加请求超时时间
 """
 
 import json
@@ -23,18 +24,20 @@ class AsyncHttpRequests(object):
     """
 
     @classmethod
-    async def get(cls, url, params=None, headers=None, decode_type='utf-8', parse_json=True):
+    async def get(cls, url, params=None, headers=None, decode_type='utf-8', parse_json=True, timeout=30):
         """ HTTP GET 请求
         @param url 请求url
         @param params 请求的uri qurey参数
+        @param headers 请求的header参数
         @param decode_type 返回body解码格式，默认使用utf-8解码
         @param parse_json 是否解析返回body为json格式，默认为True
+        @param timeout 请求超时时间，默认30秒
         @return data 返回的http body
         """
         if params:
             url = url_concat(url, params)
         http_client = AsyncHTTPClient()
-        response = await http_client.fetch(url, headers=headers)
+        response = await http_client.fetch(url, method='GET', headers=headers, request_timeout=timeout)
         if response.code != 200:
             logger.error('url:', url, 'response code:', response.code, 'response body:', response.body, caller=cls)
             raise errors.CustomError(const.ERR_MSG_INVALID)
@@ -48,22 +51,28 @@ class AsyncHttpRequests(object):
             return None
 
     @classmethod
-    async def post(cls, url, body=None, headers=None, encode_type='utf-8', decode_type='utf-8', parse_json=True):
+    async def post(cls, url, params=None, body=None, headers=None, encode_type='utf-8', decode_type='utf-8',
+                   parse_json=True, timeout=30):
         """ HTTP POST 请求
         @param url 请求url
+        @param params 请求的uri qurey参数
         @param body 请求的body参数
+        @param headers 请求的header参数
         @param encode_type 请求body编码格式，默认使用utf-8编码
         @param decode_type 返回body解码格式，默认使用utf-8解码
         @param parse_json 是否解析返回body为json格式，默认为True
+        @param timeout 请求超时时间，默认30秒
         @return data 返回的http body
         """
+        if params:
+            url = url_concat(url, params)
         if body:
             if encode_type == 'utf-8':
                 body = json.dumps(body)
             else:
                 body = urlencode(body, encoding=encode_type)
         http_client = AsyncHTTPClient()
-        response = await http_client.fetch(url, method='POST', body=body, headers=headers)
+        response = await http_client.fetch(url, method='POST', body=body, headers=headers, request_timeout=timeout)
         if response.code != 200:
             logger.error('url:', url, 'post data:', body, 'response code:', response.code, 'response body:',
                          response.body, caller=cls)
