9a25d95e886045f3815f557e80da3a429778f32b
huangtao <huangtao@klicen.com>
2017-11-13 10:38:06 +0800

    update version, add http method: head/patch/options

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ from distutils.core import setup
 
 setup(
     name='tbag',
-    version='1.1.6',
+    version='1.1.7',
 
     packages=['tbag',
               'tbag.core',
--- tbag/__init__.py
+++ tbag/__init__.py
@@ -1,4 +1,4 @@
 # -*- coding:utf-8 -*-
 
 author = 'huangtao'
-version = (1, 1, 6)
+version = (1, 1, 7)
