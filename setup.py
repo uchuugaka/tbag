5bf0baed29655686b595e23d9999908dcfcad0d5
huangtao <huangtao@ifclover.com>
2017-12-29 18:01:32 +0800

    update version 1.3.0

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ from distutils.core import setup
 
 setup(
     name='tbag',
-    version='1.2.9',
+    version='1.3.0',
     packages=['tbag',
               'tbag.core',
               'tbag.core.db',
