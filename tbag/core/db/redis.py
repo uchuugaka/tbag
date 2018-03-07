935713379f0a3d892911aae72f5914e3ddccd9e3
huangtao <huangtao@ifclover.com>
2018-03-07 15:19:48 +0800

    增加redis

--- tbag/core/db/redis.py
+++ tbag/core/db/redis.py
@@ -2,22 +2,16 @@
 
 """
 redis async操作接口
+    * Redis连接池
+    - Redis消息发布
+    - Redis消息订阅
 
 Author: huangtao
 Date:   2017/05/08
 Update: None
 """
 
-"""
-    消息发布
-    * 采用redis的publish
-"""
-
-import json
-
-import asyncio
 import aioredis
-from tornado.ioloop import IOLoop
 from tbag.utils import log as logger
 
 
@@ -25,40 +19,36 @@ __all__ = ('initRedisPool', 'RedisDBBase')
 
 
 REDIS_CONN_POOL = None  # redis连接池
-REDIS_PUB_CONN = None   # redis事件发布链接
-REDIS_SUB_CONN = None   # redis事件订阅链接
 
 
-async def initRedisPool(**kwargs):
+async def initRedisPool(host='redis://127.0.0.1:6379', db=None):
     """ 初始化连接池
     """
-    host = kwargs.get('host', 'redis://127.0.0.1:6379')
     global REDIS_CONN_POOL
-    REDIS_CONN_POOL = await aioredis.create_redis_pool(host, encoding='utf-8')
+    REDIS_CONN_POOL = await aioredis.create_redis_pool(host, db=db, encoding='utf-8')
     logger.info('create redis pool success.')
 
 
-class RedisDBBase:
-    """ redis db基类
+async def initRedisPublish(host='redis://127.0.0.1:6379', channel='test', db=None):
+    """ 初始化redis事件发布连接
     """
-
-    async def exec_cmd(self, *args, **kwargs):
-        """ 执行命令
-        """
-        result = await REDIS_CONN_POOL.execute(*args, **kwargs)
-        logger.debug('cmd:', *args, 'result:', result, caller=self)
-        return result
+    global REDIS_PUB_CONN
+    REDIS_PUB_CONN = await aioredis.create_redis(host, db=db)
+    logger.info('create redis publish channel success. channel:', channel)
 
 
+#import asyncio
+#from tornado.ioloop import IOLoop
+#
 # class RedisDBBase:
 #     """ redis连接
 #     """
 #
-#     def __init__(self):
+#     def __init__(self, host='redis://127.0.0.1:6379', channel='test'):
 #         self.pool = None    # 连接池
 #         self.pub_conn = None    # publish连接
-#         self.host = REDIS_CONFIG.get('host')
-#         self.channel = REDIS_CONFIG.get('channel')
+#         self.host = host
+#         self.channel = channel
 #
 #     async def start(self):
 #         await self._init_pool()
@@ -101,4 +91,8 @@ class RedisDBBase:
 #             data = json.loads(msg)
 #             IOLoop.current().add_callback(WebsocketHandler.push_message, data)
 #             # logger.debug('receive data:', data, caller=self)
-
+#
+#
+# redis_srv = RedisDBBase()
+# ioloop.run_until_complete(redis_srv.start())
+#
