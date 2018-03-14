17b45840d795dc9bea2f6a1039b946bb8cae944c
huangtao <huangtao@ifclover.com>
2018-03-14 12:55:24 +0800

    add http router example

--- examples/router/api.py
+++ examples/router/api.py
@@ -8,24 +8,24 @@ from tbag.utils import log as logger
 @route('/api/test', 'api.test')
 class TestHandler(WebHandler):
 
-    async def get(self, *args, **kwargs):
+    async def _get_(self, *args, **kwargs):
         logger.info('test handler GET:', self.request.path, caller=self)
         result = {'ok': 1}
         self.do_success(result)
 
-    async def post(self, *args, **kwargs):
+    async def _post_(self, *args, **kwargs):
         body = self.get_body()
         logger.info('test handler POST:', self.request.path, 'body:',  body, caller=self)
         result = {'ok': 1}
         self.do_success(result)
 
-    async def put(self, *args, **kwargs):
+    async def _put_(self, *args, **kwargs):
         body = self.get_body()
         logger.info('test handler PUT:', self.request.path, 'body:',  body, caller=self)
         result = {'ok': 1}
         self.do_success(result)
 
-    async def patch(self, *args, **kwargs):
+    async def _patch_(self, *args, **kwargs):
         body = self.get_body()
         logger.info('test handler PATCH:', self.request.path, 'body:',  body, caller=self)
         result = {'ok': 1}
